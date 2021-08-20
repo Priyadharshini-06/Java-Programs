01f89dc3db825cfe3fe569ef408ea2bcb618085d
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2021-08-20 13:50:13 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,9 @@
+1.class Student1{
+2.int id;//data member (also instance variable)
+3.String name;//data member(also instance variable)
+4.public static void main(String args[]){
+5.Student1 s1=new Student1();//creating an object of Student
+6.System.out.println(s1.id);
+7.System.out.println(s1.name);
+8.}
+9.}
