ea9fef6fcab9d256356b9e65000db348f2ffc579
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2021-08-27 13:30:01 +0530

    Add files via upload

---
+++ Program9.java
@@ -0,0 +1,9 @@
+1.class Overloading1{
+2.public static void main(int a){
+3.System.out.println(a);
+4.}
+5.public static void main(String args[]){
+6.System.out.println("main() method invoked");
+7.main(10);
+8.}
+9.}
