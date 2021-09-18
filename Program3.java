af2cd64d648619bd92714420abc87b7a97492825
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2021-09-18 15:54:42 +0530

    Update Program3.java

--- Program3.java
+++ Program3.java
@@ -1,23 +1,23 @@
-1.class Student2{
-2.int rollno;
-3.String name;
-4.
-5.void insertRecord(int r,String n){//method
-6.rollno=r;
-7.name=n;
-8.}
-9.
-10.void displayInformation(){System.out.println(rollno+" "+name);}//method
-11.
-12.public static void main(String args[]){
-13.Student2 s1=new Student2();
-14.Student2 s2=new Student2();
-15.
-16.s1.insertRecord(111,"Karan");
-17.s2.insertRecord(222,"Aryan");
-18.
-19.s1.displayInformation();
-20.s2.displayInformation();
-21.
-22.}
-23.}   
+ public	class Student2{  
+	 int rollno;  
+	 String name;  
+	  
+	 void insertRecord(int r, String n){  //method  
+	  rollno=r;  
+	  name=n;  
+	 }  
+	  
+	 void displayInformation(){System.out.println(rollno+" "+name);}//method  
+	  
+	 public static void main(String args[]){  
+	  Student2 s1=new Student2();  
+	  Student2 s2=new Student2();  
+	  
+	  s1.insertRecord(108,"Priya");  
+	  s2.insertRecord(100,"Akshaya");  
+	  s1.displayInformation();  
+
+	  s2.displayInformation();  
+	  
+	 }  
+	}  
