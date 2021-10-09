686433c504b2f063c37365fe22a941801d371d95
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2021-10-09 14:58:14 +0530

    Update Program9.java

--- Program9.java
+++ Program9.java
@@ -1,9 +1,9 @@
-1.class Overloading1{
-2.public static void main(int a){
-3.System.out.println(a);
-4.}
-5.public static void main(String args[]){
-6.System.out.println("main() method invoked");
-7.main(10);
-8.}
-9.}
+public class Overloading1{
+public static void main(int a){
+System.out.println(a);
+}
+public static void main(String args[]){
+System.out.println("main() method invoked");
+main(10);
+}
+}
