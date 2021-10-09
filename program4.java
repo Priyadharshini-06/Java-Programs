012e6066a6a55006975cb79580f1bd694441a16c
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2021-10-09 14:54:02 +0530

    Update program4.java

--- program4.java
+++ program4.java
@@ -1,22 +1,17 @@
-1.class Rectangle{
-2.int length;
-3.int width;
-4.
-5.void insert(int l,int w){
-6.length=l;
-7.width=w;
-8.}
-9.
-10.void calculateArea(){System.out.println(length*width);}
-11.
-12.public static void main(String args[]){
-13.Rectangle r1=new Rectangle();
-14.Rectangle r2=new Rectangle();
-15.
-16.r1.insert(11,5);
-17.r2.insert(3,15);
-18.
-19.r1.calculateArea();
-20.r2.calculateArea();
-21.}
-22.}
+public class Rectangle{
+int length;
+int width;
+void insert(int l,int w){
+length=l;
+width=w;
+}
+void calculateArea(){System.out.println(length*width);}
+public static void main(String args[]){
+Rectangle r1=new Rectangle();
+Rectangle r2=new Rectangle();
+r1.insert(11,5);
+r2.insert(3,15);
+r1.calculateArea();
+r2.calculateArea();
+}
+}
