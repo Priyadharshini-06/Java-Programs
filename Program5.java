a3f3d7cbe6ac27c7f42de1a6fba3a8916e0f38ea
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2021-10-09 14:50:33 +0530

    Update Program5.java

--- Program5.java
+++ Program5.java
@@ -1,14 +1,14 @@
-1.class Calculation{
-2.
-3.void fact(int n){
-4.int fact=1;
-5.for(int i=1;i<=n;i++){
-6.fact=fact*i;
-7.}
-8.System.out.println("factorial is"+fact);
-9.}
-10.
-11.public static void main(String args[]){
-12.new Calculation().fact(5);//calling method with annonymous object
-13.}
-14.}
+public class Calculation{
+
+void fact(int n){
+int fact=1;
+for(int i=1;i<=n;i++){
+fact=fact*i;
+}
+System.out.println("factorial is "+fact);
+}
+
+public static void main(String args[]){
+new Calculation().fact(5);//calling method with annonymous object
+}
+}
