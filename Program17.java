600db22869323b5699f9a23d629b59581df46eda
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2021-10-23 14:51:55 +0530

    Update Program17.java

--- Program17.java
+++ Program17.java
@@ -1,16 +1,16 @@
-1.	class Counter2{  
-2.	static int count=0;//will get memory only once and retain its value  
-3.	  
-4.	Counter2(){  
-5.	count++;  
-6.	System.out.println(count);  
-7.	}  
-8.	  
-9.	public static void main(String args[]){  
-10.	  
-11.	Counter2 c1=new Counter2();  
-12.	Counter2 c2=new Counter2();  
-13.	Counter2 c3=new Counter2();  
-14.	  
-15.	 }  
-16.	} 
+public class Counter2{  
+	static int count=0;//will get memory only once and retain its value  
+	  
+	Counter2(){  
+	count++;  
+	System.out.println(count);  
+	}  
+	  
+	public static void main(String args[]){  
+	  
+	Counter2 c1=new Counter2();  
+	Counter2 c2=new Counter2();  
+	Counter2 c3=new Counter2();  
+	  
+	 }  
+	} 
