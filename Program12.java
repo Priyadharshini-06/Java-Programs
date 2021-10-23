7f3cdf989050ac7531e2f41e751889ab52931c8d
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2021-10-23 14:28:34 +0530

    Update Program12.java

--- Program12.java
+++ Program12.java
@@ -1,11 +1,11 @@
-1.class Student3{
-2.int id;
-3.String name;
-4.void display(){System.out.println(id+" "+name);}
-5.public static void main(String args[]){
-6.Student3 s1=new Student3();
-7.Student3 s2=new Student3();
-8.s1.display();
-9.s2.display();
-10.}
-11.}
+public class Student3{
+int id;
+String name;
+void display(){System.out.println(id+" "+name);}
+public static void main(String args[]){
+Student3 s1=new Student3();
+Student3 s2=new Student3();
+s1.display();
+s2.display();
+}
+}
