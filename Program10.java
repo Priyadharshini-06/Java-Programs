b5b8239e9becb80f8ce5a7ebd3cacb8676408efb
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2021-10-23 14:23:47 +0530

    Update Program10.java

--- Program10.java
+++ Program10.java
@@ -1,9 +1,9 @@
-1.class OverloadingCalculation1{
-2.void sum(int a,long b){System.out.println(a+b);}
-3.void sum(int a,int b,int c){System.out.println(a+b+c);}
-4.public static void main(String args[]){
-5.OverloadingCalculation1 obj=new OverloadingCalculation1();
-6.obj.sum(20,20);//now second int literal will be promoted to long
-7.obj.sum(20,20,20);
-8.}
-9.}
+public class OverloadingCalculation1{
+void sum(int a,long b){System.out.println(a+b);}
+void sum(int a,int b,int c){System.out.println(a+b+c);}
+public static void main(String args[]){
+OverloadingCalculation1 obj=new OverloadingCalculation1();
+obj.sum(20,20);//now second int literal will be promoted to long
+obj.sum(20,20,20);
+}
+}
