fbd35243cc2f0e120b14dc38923137bbb52080b9
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2021-10-23 14:59:51 +0530

    Update Program20.java

--- Program20.java
+++ Program20.java
@@ -1,16 +1,15 @@
-2.	class Student11{  
-3.	    int id;  
-4.	    String name;  
-5.	      
-6.	    Student11(int id,String name){  
-7.	    this.id = id;  
-8.	    this.name = name;  
-9.	    }  
-10.	    void display(){System.out.println(id+" "+name);}  
-11.	    public static void main(String args[]){  
-12.	    Student11 s1 = new Student11(111,"Karan");  
-13.	    Student11 s2 = new Student11(222,"Aryan");  
-14.	    s1.display();  
-15.	    s2.display();  
-16.	}  
-17.	}  
+public	class Student11{  
+	    int id;  
+	    String name;  
+	      
+	    Student11(int id,String name){  
+	    this.id = id;  
+	    this.name = name;  	    }  
+	    void display(){System.out.println(id+" "+name);}  
+	    public static void main(String args[]){  
+	    Student11 s1 = new Student11(111,"Karan");  
+	    Student11 s2 = new Student11(222,"Aryan");  
+	    s1.display();  
+	    s2.display();  
+	}  
+	}  
