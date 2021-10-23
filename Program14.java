f850d340a682f20f86f74cb3c1ebf1198396277d
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2021-10-23 14:44:56 +0530

    Update Program14.java

--- Program14.java
+++ Program14.java
@@ -1,22 +1,22 @@
-1.class Student5{
-2.int id;
-3.String name;
-4.int age;
-5.Student5(int i,String n){
-6.id=i;
-7.name=n;
-8.}
-9.Student5(int i,String n,int a){
-10.id=i;
-11.name = n;
-12.age=a;
-13.}
-14.void display(){System.out.println(id+" "+name+" "+age);}
-15.
-16.public static void main(String args[]){
-17.Student5 s1=new Student5(111,"Karan");
-18.Student5 s2=new Student5(222,"Aryan",25);
-19.s1.display();
-20.s2.display();
-21.}
-22.}
+public class Student5{
+int id;
+String name;
+int age;
+Student5(int i,String n){
+id=i;
+name=n;
+}
+Student5(int i,String n,int a){
+id=i;
+name = n;
+age=a;
+}
+void display(){System.out.println(id+" "+name+" "+age);}
+
+public static void main(String args[]){
+Student5 s1=new Student5(111,"Karan");
+Student5 s2=new Student5(222,"Aryan",25);
+s1.display();
+s2.display();
+}
+}
