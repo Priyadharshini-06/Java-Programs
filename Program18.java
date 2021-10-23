890654f31eed6322066b7e362a86cd656b41116f
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2021-10-23 14:55:20 +0530

    Update Program18.java

--- Program18.java
+++ Program18.java
@@ -1,28 +1,28 @@
-3.	class Student9{  
-4.	     int rollno;  
-5.	     String name;  
-6.	     static String college = "ITS";  
-7.	       
-8.	     static void change(){  
-9.	     college = "BBDIT";  
-10.	     }  
-11.	  
-12.	     Student9(int r, String n){  
-13.	     rollno = r;  
-14.	     name = n;  
-15.	     }  
-16.	  
-17.	     void display (){System.out.println(rollno+" "+name+" "+college);}  
-18.	  
-19.	    public static void main(String args[]){  
-20.	    Student9.change();  
-21.	  
-22.	    Student9 s1 = new Student9 (111,"Karan");  
-23.	    Student9 s2 = new Student9 (222,"Aryan");  
-24.	    Student9 s3 = new Student9 (333,"Sonoo");  
-25.	  
-26.	    s1.display();  
-27.	    s2.display();  
-28.	    s3.display();  
-29.	    }  
-30.	}  
+public	class Student9{  
+	     int rollno;  
+	     String name;  
+	     static String college = "ITS";  
+	       
+	     static void change(){  
+	     college = "BBDIT";  
+	     }  
+	  
+	     Student9(int r, String n){  
+	     rollno = r;  
+	     name = n;  
+	     }  
+	  
+	     void display (){System.out.println(rollno+" "+name+" "+college);}  
+	  
+	    public static void main(String args[]){  
+        Student9.change();  
+	  
+	    Student9 s1 = new Student9 (111,"Priya");  
+	    Student9 s2 = new Student9 (222,"Akshaya");  
+	    Student9 s3 = new Student9 (333,"Srinidhi");  
+	  
+	    s1.display();  
+	    s2.display();  
+	    s3.display();  
+	    }  
+	}  
