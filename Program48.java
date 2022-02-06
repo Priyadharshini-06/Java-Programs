ae26dda58ed5effc603c81c6fc0c3713f1857f38
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2022-02-06 12:58:31 +0530

    Add files via upload

---
+++ Program48.java
@@ -0,0 +1,35 @@
+interface H{  
+	void a();//bydefault, public and abstract  
+	void b();  
+	void c();  
+	void d();  
+	}  
+	  
+	//Creating abstract class that provides the implementation of one method of A interface  
+	abstract class P implements H{  
+	public void c(){System.out.println("I am C");
+    
+    }  
+	}  
+	  
+	//Creating subclass of abstract class, now we need to provide the implementation of rest of the methods  
+	class M extends P{  
+	public void a(){System.out.println("I am a");
+    }  
+	public void b(){System.out.println("I am b");
+    }  
+	public void d(){System.out.println("I am d");
+    
+    }  
+	}  
+	//Creating a test class that calls the methods of A interface  
+	public class Test5{  
+	public static void main(String args[]){  
+	H a=new M();  
+	a.a();  
+	a.b();  
+	a.c();  
+	a.d();  
+	}
+    
+    }
