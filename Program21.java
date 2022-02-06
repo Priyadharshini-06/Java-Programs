19fe09fb6b66bb24df27b005dc2ee4111109a1d0
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2022-02-06 11:12:53 +0530

    Update Program21.java

--- Program21.java
+++ Program21.java
@@ -1,19 +1,21 @@
-3.	class Student13{  
-4.	    int id;  
-5.	    String name;  
-6.	    Student13(){System.out.println("default constructor is invoked");}  
-7.	      
-8.	    Student13(int id,String name){  
-9.	    this ();//it is used to invoked current class constructor.  
-10.	    this.id = id;  
-11.	    this.name = name;  
-12.	    }  
-13.	    void display(){System.out.println(id+" "+name);}  
-14.	      
-15.	    public static void main(String args[]){  
-16.	    Student13 e1 = new Student13(111,"karan");  
-17.	    Student13 e2 = new Student13(222,"Aryan");  
-18.	    e1.display();  
-19.	    e2.display();  
-20.	   }  
-21.	}  
+public class Student13{  
+	    int id;  
+	    String name;  
+	    Student13(){
+	        System.out.println("default constructor is invoked");
+	    }  
+	    Student13(int id,String name){  
+	    this ();//it is used to invoked current class constructor.  
+	    this.id = id;  
+	    this.name = name;  
+	    }  
+	    void display(){
+	        System.out.println(id+" "+name);
+	    }  
+	    public static void main(String args[]){  
+	    Student13 e1 = new Student13(111,"karan");  
+	    Student13 e2 = new Student13(222,"Aryan");  
+	    e1.display();  
+	    e2.display();  
+	   }  
+	}  
