48e2a55c1fe18f6f9c59cf9f3d5ca89b28179501
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2022-02-06 12:14:19 +0530

    Add files via upload

---
+++ Program35.java
@@ -0,0 +1,18 @@
+//super can be used to invoke parent class method
+	class Person1{  
+	void message(){System.out.println("Welcome");}  
+	}  
+	  
+	 public class Student1 extends Person1{  
+	void message(){System.out.println("welcome to java course");}  
+	  
+	void display(){  
+	message();//will invoke current class message() method  
+	super.message();//will invoke parent class message() method  
+	}  
+	  
+	public static void main(String args[]){  
+	Student1 s=new Student1();  
+	s.display();  
+	}  
+	}
