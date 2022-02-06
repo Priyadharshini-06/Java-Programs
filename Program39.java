affea0a49b86ee9cd6d481bb092e1cc691f0a673
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2022-02-06 12:39:19 +0530

    Add files via upload

---
+++ Program39.java
@@ -0,0 +1,9 @@
+//static binding
+	public class Monkey{  
+	 private void jump(){System.out.println("Monkey is Jumping...");}  
+	  
+	 public static void main(String args[]){  
+	  Monkey m =new Monkey();  
+	  m.jump();  
+	 }  
+	}
