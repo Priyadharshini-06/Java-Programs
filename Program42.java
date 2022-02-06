3b629a9e7a88e12edfa70adbf12e286e9466661f
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2022-02-06 12:45:40 +0530

    Add files via upload

---
+++ Program42.java
@@ -0,0 +1,7 @@
+//Simple example of java instanceof
+	public class Simple{  
+	 public static void main(String args[]){  
+	 Simple h = new Simple();  
+	 System.out.println(h instanceof Simple);//true  
+	 }  
+	}
