00a1e647ea1aa550e2a48c21928be3e4a7322d99
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2022-02-06 11:18:34 +0530

    Update Program24.java

--- Program24.java
+++ Program24.java
@@ -1,13 +1,12 @@
-1.	class S2{  
-2.	  void m(S2 obj){  
-3.	  System.out.println("method is invoked");  
-4.	  }  
-5.	  void p(){  
-6.	  m(this);  
-7.	  }  
-8.	    
-9.	  public static void main(String args[]){  
-10.	  S2 s1 = new S2();  
-11.	  s1.p();  
-12.	  }  
-13.	}  
+public class S2{  
+	  void m(S2 obj){  
+	  System.out.println("method is invoked");  
+	  }  
+	  void p(){  
+	  m(this);  
+	  }  
+	  public static void main(String args[]){  
+	  S2 s1 = new S2();  
+	  s1.p();  
+	  }  
+	} 
