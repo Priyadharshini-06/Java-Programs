4033de40b91fbe35668b0ef43c2573295dd4eb0c
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2022-02-06 11:17:14 +0530

    Update Program23.java

--- Program23.java
+++ Program23.java
@@ -1,15 +1,15 @@
-1.	class S{  
-2.	  void m(){  
-3.	  System.out.println("method is invoked");  
-4.	  }  
-5.	  void n(){  
-6.	  this.m();//no need because compiler does it for you.  
-7.	  }  
-8.	  void p(){  
-9.	  n();//complier will add this to invoke n() method as this.n()  
-10.	  }  
-11.	  public static void main(String args[]){  
-12.	  S s1 = new S();  
-13.	  s1.p();  
-14.	  }  
-15.	}  
+public class S{  
+	  void m(){  
+	  System.out.println("method is invoked");  
+	  }  
+	  void n(){  
+	  this.m();//no need because compiler does it for you.  
+	  }  
+	  void p(){  
+	  n();//complier will add this to invoke n() method as this.n()  
+	  }  
+	  public static void main(String args[]){  
+	  S s1 = new S();  
+	  s1.p();  
+	  }  
+	}  
