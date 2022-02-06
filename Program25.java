23a3cfc5748820b98577acb4a47ef83a5aec2b27
Priyadharshini-06 <89245134+Priyadharshini-06@users.noreply.github.com>
2022-02-06 11:21:31 +0530

    Update Program25.java

--- Program25.java
+++ Program25.java
@@ -1,20 +1,19 @@
-1.	class B{  
-2.	  A4 obj;  
-3.	  B(A4 obj){  
-4.	    this.obj=obj;  
-5.	  }  
-6.	  void display(){  
-7.	    System.out.println(obj.data);//using data member of A4 class  
-8.	  }  
-9.	}  
-10.	  
-11.	class A4{  
-12.	  int data=10;  
-13.	  A4(){  
-14.	   B b=new B(this);  
-15.	   b.display();  
-16.	  }  
-17.	  public static void main(String args[]){  
-18.	   A4 a=new A4();  
-19.	  }  
-20.	}  
+class B{  
+	  A4 obj;  
+	  B(A4 obj){  
+	    this.obj=obj;  
+	  }  
+	  void display(){  
+	    System.out.println(obj.data);//using data member of A4 class  
+	  }  
+	}  
+	public class A4{  
+	  int data=10;  
+	  A4(){  
+	   B b=new B(this);  
+	   b.display();  
+	  }  
+	  public static void main(String args[]){  
+	   A4 a=new A4();  
+	  }  
+	}  
